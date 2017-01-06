0bc76104e02af646a98e9c2f745215632c5304dd
lsyu <lsyu@mobvoi.com>
2017-01-06 15:55:27 +0800

    init empty project
    Signed-off-by: lsyu <lsyu@mobvoi.com>

---
+++ app/src/main/java/com/yls/pmlist/pm_list/ListPackageActivity.java
@@ -0,0 +1,13 @@
+package com.yls.pmlist.pm_list;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListPackageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_package);
+    }
+}
